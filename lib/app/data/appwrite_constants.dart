c69baa73b75268abea771da3c4bcd223110a9a47
bigbott <u.novicow@gmail.com>
2025-07-30 16:56:41 +0300

    new

---
+++ lib/app/data/appwrite_constants.dart
@@ -0,0 +1,13 @@
+
+ final class AppwriteConstants {
+   AppwriteConstants._();
+   static const PROJECT_ID = '6861211500017054125c'; 
+   static const DB_ID = '6867866f0015673602ab';
+   static const COLLECTION_ID_7X7 = '686787330017241efa6b';
+   static const COLLECTION_ID_8X8 = '';
+   static const COLLECTION_ID_8X10 = '';
+   static const COLLECTION_ID_STATS = '687fb5850007dd1381ab';
+   static const FUNCTION_ID = '6870f68a001d3ee06258';
+   static const ENDPOINT = 'https://cloud.appwrite.io/v1';
+   
+}
